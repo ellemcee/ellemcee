f77d77b124099423b98c4af0b41b61fc6c4b5c68
Lauren Cain <ellezuhause@gmail.com>
2017-06-28 13:41:53 -0600

    gulp-ruby-sass setup plus dependencies; gulp-watch installed but no task added to gulpfile; changed directory structure and naming

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,62 @@
-var gulp = require('gulp');
+'use strict'
 
-gulp.task('default', function() {
-  // place code for your default task here
+var gulp = require('gulp'),
+    sass = require('gulp-ruby-sass');
+
+var controller = 'a/s/styles.scss',
+    path = 'a/s/';
+
+// dev
+gulp.task('sass', function(){
+	return sass(
+			controller,
+			{
+				style: 'nested', // nested, compact, compressed, expanded
+				sourcemap: true,
+			}
+		)
+    	.on('error', function (err) {
+      		console.error('Error!', err.message);
+   		})
+    	.pipe(gulp.dest(path));
 });
+// dist
+gulp.task('sass-dist', function() {
+    return sass(
+            controller, {
+                style: 'compressed', // nested, compact, compressed, expanded
+                sourcemap: false,
+            }
+        )
+        .on('error', function(err) {
+            console.error('Error!', err.message);
+        })
+        .pipe(autoprefixer({
+            browsers: ['> 5%', 'last 5 versions'],
+            cascade: true
+        }))
+        .pipe(gulp.dest(path));
+});
+// run
+gulp.task('watch', function() {
+    gulp.watch( path + '**/*.scss', ['sass'] );
+});
+
+gulp.task('help', function() {
+    console.log('========== HELP ===========');
+    console.log('');
+    console.log('');
+    console.log('Available Commands:');
+    console.log('');
+    console.log('- gulp');
+    console.log('The default task will run watch on the /a/s/* directory looking for any changes, modifications, additions, or deletions involving .scss files. If a change has happened it will execute the sass task.');
+    console.log('');
+    console.log('- gulp dist');
+    console.log('The sass task will compile core.scss located in the /a/s/* directory with distribution spec. You can run gulp sass for manual compiling');
+    console.log('');
+    console.log('');
+    console.log('======== END HELP =========');
+});
+
+gulp.task('default', ['watch', 'sass']);
+gulp.task('dist', ['sass-dist']);
--- index.php
+++ index.php
@@ -1,1 +1,9 @@
-<h1>Hello</h1>
+<html>
+<head>
+  <link rel="stylesheet" href="/a/s/styles.css" type="text/css">
+</head>
+<body>
+<h1>Hello, Kolya</h1>
+<h1 class="test">Hello, Kolya</h1>
+</body>
+</html>
